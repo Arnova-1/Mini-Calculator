b66d77776d6ff7ca0f44fdfd40d22d011d7f2710
Arnova-1 <arnova124@gmail.com>
2025-10-30 22:52:03 +0700

    Refactor the prompt function

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,17 @@ fn main() {
     println!("───────────────────\n║ MINI-CALCULATOR ║\n───────────────────");
     println!("Select an operation:\n[1] Addition(+)\n[2] Subtraction(-)\n[3] Multiplication(*)\n[4] Division(÷)\n[0] Exit");
 
+
+
     loop {
-        print!("> ");
-        io::stdout().flush().unwrap();
-
-        let mut input = String::new();
-        match io::stdin().read_line(&mut input) {
-            Ok(input) => input,
-            Err(e) => {
-                println!("Failed to read line: {e}");
-                continue;
-            }
-        };
-
-        let input: i32 = match input.trim().parse() {
-            Ok(num) => num,
-            Err(e) => {
-                println!("{e}. Please enter a number!");
-                continue;
-            }
-        };
+        let input = prompt();
 
         match input {
             0 => break,
             1 | 2 | 3 | 4 => todo!(),
             _ => {
-                println!("Invalid choice. Choose an operation by typing 0 - 4!")
+                println!("Invalid choice. Choose an operation by typing 0 - 4!");
+                continue;
             }
         }
     }
@@ -51,5 +36,30 @@ impl Operation {
             Operation::Multiplication => a * b,
             Operation::Division => a / b,
         }
+    }
+}
+
+fn prompt() -> i32 {
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(input) => input,
+            Err(e) => {
+                println!("Failed to read line: {e}");
+                continue;
+            }
+        };
+
+        let input: i32 = match input.trim().parse() {
+            Ok(input) => input,
+            Err(e) => {
+                println!("{e}. Please enter a number!");
+                continue;
+            }
+        };
+        return input;
     }
 }
